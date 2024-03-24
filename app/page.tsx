9a69bdb42e11e61d59ca32a5d269bf914daea34d
ppaanngggg <hantian.pang@gmail.com>
2024-03-24 22:12:46 +0800

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import { MdOutlineSearch } from "react-icons/md";
+import Records from "@/app/results/records";
+
+// define a structure for the data
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
       <label className="input input-bordered flex items-center w-6/12">
         <MdOutlineSearch className="" />
         <input type="text" className="grow" placeholder="Search" />
       </label>
+      <Records />
     </main>
   );
 }
