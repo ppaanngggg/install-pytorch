b2c8c7bda9642b8a7541c79efe556f988cecf9dd
ppaanngggg <hantian.pang@gmail.com>
2024-03-29 23:40:05 +0800

    add analytics and speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { clsx } from "clsx";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" data-theme="bumblebee">
+      <head lang="en"></head>
       <body className={clsx("subpixel-antialiased", inter.className)}>
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
