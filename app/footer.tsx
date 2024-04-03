e53cf236a71dab94c0f8c46348e49b4c0587a030
ppaanngggg <hantian.pang@gmail.com>
2024-04-03 22:57:18 +0800

    add robots and sitemap and copyright

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,5 +1,10 @@
 export default function Footer() {
   return (
-    <footer className="flex flex-row items-center justify-center w-full h-16 footer-background text-white"></footer>
+    <footer className="flex flex-row items-center justify-center w-full h-20 footer-background text-white">
+      {/*add copyright 2024 for ppanngggg here*/}
+      <div className="flex flex-row items-center justify-center">
+        <span className="font-semibold">© 2024 ppanngggg</span>
+      </div>
+    </footer>
   );
 }
